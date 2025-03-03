7c0b3d2eecbf970962f91bd2568668c90d080462
Lukasz Piwowarski <lukaszp4@onet.eu>
2025-03-03 23:31:18 +0100

    added loggers and auxiliary methods

--- src/main/java/com/example/postendpoint/ShawnController.java
+++ src/main/java/com/example/postendpoint/ShawnController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -22,20 +22,31 @@ public class ShawnController {
     @PostMapping(path = "/shawn/songs")
     public ResponseEntity<ResponseBodyShawn> postSmth(@RequestBody RequestBodyShawn requestBodyShawn) {
         String songName = requestBodyShawn.songName();
+        log.info(songName);
         songs.add(songName);
         return ResponseEntity.ok(new ResponseBodyShawn("Added new Song!", List.of(songName)));
     }
 
     @GetMapping(path = "/shawn/songs")
-    public ResponseEntity<ResponseBodyShawn> getAllSongs() {
+    public ResponseEntity<ResponseBodyShawn> getAllSongs(@RequestHeader(required = false) String someOtherName,
+                                                         @RequestHeader(required = false) String connection) {
+        String toStringWithIndexes = toStringWithIndexes(songs);
         System.out.println(songs);
-        return ResponseEntity.ok(new ResponseBodyShawn("songs", Collections.emptyList()));
+        return ResponseEntity.ok(new ResponseBodyShawn(toStringWithIndexes + "requestedId was: " + connection, songs));
     }
 
     @DeleteMapping(path = "/shawn/songs/{id}")
     public ResponseEntity<String> deleteShawn(@PathVariable int id) {
         songs.remove(id);
         return ResponseEntity.ok("You deleted shawn song with id: " + id);
+    }
+
+    private <T> String toStringWithIndexes(List<T> list) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(i).append(": ").append(list.get(i)).append(" ");
+        }
+        return sb.toString();
     }
 
 }
